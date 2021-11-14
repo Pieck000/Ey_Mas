f693a142f3f7e29fbbe306281e4ad905ac25dacd
Pieck000 <77354417+Pieck000@users.noreply.github.com>
2021-11-14 14:03:16 -0600

    Avance Interfaces

---
+++ app/src/main/java/com/isproject/ey/EventoActivity.java
@@ -0,0 +1,14 @@
+package com.isproject.ey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento);
+    }
+}

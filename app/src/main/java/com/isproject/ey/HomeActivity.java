804e63f06e9deadeed6d6fda494f5ae58763c967
Pieck000 <77354417+Pieck000@users.noreply.github.com>
2021-11-25 21:06:32 -0600

    Login

--- app/src/main/java/com/isproject/ey/HomeActivity.java
+++ app/src/main/java/com/isproject/ey/HomeActivity.java
@@ -1,14 +1,66 @@
 package com.isproject.ey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
-
+    TextView tvPr;
+    //Fire
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        iniciarFB();
+        tvPr = findViewById(R.id.tvPr);
+
+        try{
+            String id = mAuth.getCurrentUser().getUid();
+            databaseReference.child("Usuario").child(id).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        String name = snapshot.child("nombre").getValue().toString();
+                        tvPr.setText("Usu: "+name);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull  DatabaseError error) {
+
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(HomeActivity.this, ""+e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+    private void iniciarFB()
+    {
+        try
+        {
+            FirebaseApp.initializeApp(this);
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            databaseReference = firebaseDatabase.getReference();
+            mAuth = FirebaseAuth.getInstance();
+        }catch(Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/isproject/ey/LoginActivity.java
+++ app/src/main/java/com/isproject/ey/LoginActivity.java
@@ -1,14 +1,97 @@
 package com.isproject.ey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.isproject.ey.firebase.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btLogin, btBack;
+    EditText etCorreo, etPass;
+    //Fire
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        iniciarFB();
+        etCorreo = findViewById(R.id.edit_correo_lg);
+        etPass = findViewById(R.id.edit_password_lg);
+
+
+        btLogin = findViewById(R.id.bt_login_lg);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mValidar()){
+                    try {
+                        String correo = etCorreo.getText().toString();
+                        String pass = etPass.getText().toString();
+                        mAuth.signInWithEmailAndPassword(correo,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(LoginActivity.this, "Contraseña o Correo electronico erroneo", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }catch (Exception e){
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        btBack = findViewById(R.id.bt_back_lg);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(LoginActivity.this, "atras", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+    private boolean mValidar(){
+        if(etCorreo.getText().toString().equals("")||etPass.getText().toString().equals("")){
+            return false;
+        }else {
+            return true;
+        }
+    }
+    private void iniciarFB()
+    {
+        try
+        {
+            FirebaseApp.initializeApp(this);
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            databaseReference = firebaseDatabase.getReference();
+            mAuth = FirebaseAuth.getInstance();
+        }catch(Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }

38b2802f89353a60169737813f34e12ae9222bb9
Pieck000 <77354417+Pieck000@users.noreply.github.com>
2021-12-06 13:23:57 -0600

    Cadenero QR scan

---
+++ app/src/main/java/com/isproject/ey/ui/cadenero/CadeneroFragment.java
@@ -0,0 +1,82 @@
+package com.isproject.ey.ui.cadenero;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.isproject.ey.R;
+
+
+public class CadeneroFragment extends Fragment {
+
+    private CadeneroViewModel mViewModel;
+
+    public static CadeneroFragment newInstance() {
+        return new CadeneroFragment();
+    }
+    //
+    Button btScan;
+    TextView tvScan;
+    //IntentResult result;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_cadenero, container, false);
+        //
+        tvScan = root.findViewById(R.id.tv_scan);
+        btScan = root.findViewById(R.id.bt_scan);
+        //
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(getActivity());
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Ey+, Escanea una Invitacion");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.forSupportFragment(CadeneroFragment.this).initiateScan();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                tvScan.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CadeneroViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/isproject/ey/ui/cadenero/CadeneroViewModel.java
@@ -0,0 +1,7 @@
+package com.isproject.ey.ui.cadenero;
+
+import androidx.lifecycle.ViewModel;
+
+public class CadeneroViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
